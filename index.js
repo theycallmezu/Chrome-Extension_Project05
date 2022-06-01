2b90ca3e4da4bb25f8c871bc6539e7716876643b
zu <105687726+theycallmezu@users.noreply.github.com>
2022-05-31 19:23:14 -0500

    Project finished

--- index.js
+++ index.js
@@ -1,77 +1,47 @@
-
-
-/*
-
-// Minimal Button 01
-
-let inputBtn = document.getElementById("input-btn")
-
-inputBtn.addEventListner("click", function() {
-    console.log("Button clicked from addEventListener")
-})
-
-
-// Minimal Button 02
-
-let inputButton = document.getElementById("input-btn");
-
-inputButton.addEventListener( "click", saveLead() )
-
-function saveLead() {
-    console.log("Button clicked!")
-}
-
-*/
-
-
-// Array Collector
-
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    inputEl.value = " ";
-    renderLeads()
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
 })
-
-
-/*
-for (let i = 0; i < myLeads.length; i++) {
-    ulEl.textContent += myLeads[i] + " "
-}
-
-
-
-for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-    // create element
-    // set text content
-    // append to ul
-    const li = document.createElement("li")
-    li.textContent = myLeads[i]
-    ulEl.append(li)
-}
-
-
-
-for (let i=0; i < myLeads.length; i++) {
-
-    const li = document.createElement("li")
-    li.textContent = myLeads[i]
-    ulEl.append(li)
-
-} 
-
-*/
-
-function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>"
-    }
-     ulEl.innerHTML = listItems
-}
-
